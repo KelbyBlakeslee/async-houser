8aadb50236cb264c0acfb5101a001a8cb97fe9a6
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-07-09 10:04:27 -0600

    database connection

--- server/index.js
+++ server/index.js
@@ -7,3 +7,19 @@ const express = require('express')
 
 const app = express();
 app.use(bodyParser.json())
+
+const {
+    CONNECTION_STRING,
+    SERVER_PORT
+} = process.env
+
+//massive connections 
+massive(CONNECTION_STRING).then((db) => {
+    console.log('db connected')
+    app.set('db', db)
+});
+
+//SERVER
+app.listen(SERVER_PORT, () => {
+    console.log(`We are many, You are one on ${SERVER_PORT}`)
+});
