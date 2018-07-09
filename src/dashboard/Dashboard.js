4fcc58770e4f7c49def851294151210b2421f2e6
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-07-09 14:57:16 -0600

    adding needed components

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Dashboard extends Component {
+    constructor() {
+        super();
+
+
+
+    }
+
+
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+
+export default Dashboard;
